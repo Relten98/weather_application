f7159b08386af80ff4f879dfd9c48e6964429e2e
Relten98 <73558735+Relten98@users.noreply.github.com>
2020-12-18 19:51:16 -0800

    update: script.js, index.html
    Properly linked to the API, now all that is needed is to get proper user interaction.

--- assets/script.js
+++ assets/script.js
@@ -1,39 +1,60 @@
 /// Weather API setup. Uses the NOAA API, as it it better than open weather, and far more reliable.
-
 const settings = {
     "async": true,
     "crossDomain": true,
-    "url": "https://national-weather-service.p.rapidapi.com/products/types/undefined",
+    "url": "https://community-open-weather-map.p.rapidapi.com/weather?q=London%2Cuk&lat=0&lon=0&callback=test&id=2172797&lang=null&units=%22metric%22%20or%20%22imperial%22&mode=xml%2C%20html",
     "method": "GET",
     "headers": {
         "x-rapidapi-key": "472018e923msha4370e43be7a558p159d37jsn6c2d4e161c97",
-        "x-rapidapi-host": "national-weather-service.p.rapidapi.com"
+        "x-rapidapi-host": "community-open-weather-map.p.rapidapi.com"
     }
 };
+
+$.ajax(settings).done(function(response) {
+    console.log(response);
+});
+
+
+// When initialized, elements are hidden until after load.
+function hideElements() {
+    $('.content').hide;
+};
+
+function showElements() {
+    $('.content').show;
+}
+hideElements();
 
 /// Sets the visual image depending on the current weather.
 var weatherIcon = []
 
 
 // Preloader set up
-
-$.ajax(settings).done(function(response) {
-    console.log(response);
-});
 $(document).ready(function() {
 
-    //Preloader info
+    //Hides the preloader when the page is loaded, and shows the content.
 
-    preloaderFadeOutTime = 500;
+    preloaderFadeOutTime = 1000;
 
     function hidePreloader() {
         var preloader = $('.preloader');
         preloader.fadeOut(preloaderFadeOutTime);
     }
+
+
     hidePreloader();
+    showElements();
 });
-/* 
+
+
+
+
+
+
+/* Info for the loading animmation:
+
   Loading animation for Authentic Weather ~ http://authenticweather.com
   -- Made in collaboration with Tobias van Schneider :
   -- http://www.vanschneider.com/
-*/
+
+  */
