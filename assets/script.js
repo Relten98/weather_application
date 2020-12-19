1d508c47c447a481c8736befce21c352ea72df51
Relten98 <73558735+Relten98@users.noreply.github.com>
2020-12-19 10:13:17 -0800

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,18 @@
-function weatherBalloon(cityID) {
+var date = new Date();
+
+var month = date.getMonth() + 1;
+var day = date.getDate();
+
+var output = date.getFullYear() + '/' +
+    (month < 10 ? '0' : '') + month + '/' +
+    (day < 10 ? '0' : '') + day;
+
+let cityID = 'london'
+    /// Weather maker
+
+function getWeather(cityID) {
     var key = 'e0efbfd98a4c5bc87309c276d2b36cd9';
-    fetch('https://api.openweathermap.org/data/2.5/weather?id=' + cityID + '&appid=' + key)
+    fetch('https://api.openweathermap.org/data/2.5/weather?q=' + cityID + '&appid=' + key)
         .then(function(resp) { return resp.json() }) // Convert data to json
         .then(function(data) {
             drawWeather(data); // Call drawWeather
@@ -12,10 +24,10 @@ function weatherBalloon(cityID) {
 }
 
 window.onload = function() {
-    weatherBalloon(6167865);
+    getWeather(cityID);
 }
 
-function drawWeather(d) {
+function showWeather(d) {
     var celcius = Math.round(parseFloat(d.main.temp) - 273.15);
     var fahrenheit = Math.round(((parseFloat(d.main.temp) - 273.15) * 1.8) + 32);
 
