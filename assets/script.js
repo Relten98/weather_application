659fcc18f67e1c9cf1b1b2838e4228314e250fd4
Relten98 <diezhandinoty@gmail.com>
2021-03-04 19:25:07 -0800

    updated and cleaned up script.js
    cityname now properly displays and prints a value, which should allow the application to properly function.
    
    Saving information is in the works, but beyond that, the application should be more than functional, aside from providing proper forecast information,
    and saving previous searches to reduce api calls.

--- assets/script.js
+++ assets/script.js
@@ -1,40 +1,49 @@
 /// City id changed with input in HTML
 var cityName = document.getElementById('cityInput').value
-
-
-/// magic key
+/// Magic key
 const key = 'e0efbfd98a4c5bc87309c276d2b36cd9';
 
-/// loads the weather api & forcast
-var weatherURL = 'https://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&appid=' + key
-var forcastURL = 'https://api.openweathermap.org/data/2.5/forecast?q=' + cityName + '&appid=' + key
-
-
-
-/*
-// Lat and lon along with uv
-var lat = response.coord.lat;
-var lon = response.coord.lon;
-var findUVURL = "https://api.openweathermap.org/data/2.5/uvi?&appid=" + key + "&lat=" + lat + "&lon=" + lon;
-*/
 
 //// Current time
 let date = moment().format('L');
 
 //// Get city
 function findWeather(cityName) {
+
+    /// loads the weather api & forcast
+var weatherURL = 'https://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&appid=' + key
+
     fetch(weatherURL)
-        .then(function(resp) { return resp.json() }) // Convert data to json
-        .then(function(data) {
+        .then(function (resp) { return resp.json() }) // Convert data to json
+        .then(function (data) {
             showWeather(data);
             console.log(data)
         })
-        .catch(function() {
+        .catch(function () {
             // catch any errors
         });
 }
 
-window.onload = function() {
+function findForecast(cityName) {
+    var forcastURL = 'https://api.openweathermap.org/data/2.5/forecast?q=' + cityName + '&appid=' + key
+
+
+}
+
+/*
+
+// Lat and lon along with uv
+var lat = response.coord.lat;
+
+var lon = response.coord.lon;
+
+var findUVURL = "https://api.openweathermap.org/data/2.5/uvi?&appid=" + key + "&lat=" + lat + "&lon=" + lon;
+
+*/
+
+
+
+window.onload = function () {
     var lastSearch = JSON.parse(localStorage.getItem("cityInput"));
     var searchDiv = $("<button class='btn border text-muted mt-1 shadow-sm bg-white rounded' style='width: 12rem;'>").text(lastSearch);
     var psearch = $("<div>");
@@ -44,19 +53,11 @@ window.onload = function() {
 }
 
 function showWeather(d) {
-    var celcius = Math.round(parseFloat(d.main.temp) - 273.15);
-    var fahrenheit = Math.round(((parseFloat(d.main.temp) - 273.15) * 1.8) + 32);
 
-    document.getElementById('description').innerHTML = d.weather[0].description;
-    document.getElementById('temp').innerHTML = celcius + '&deg;';
-    document.getElementById('location').innerHTML = d.name;
-    document.getElementById('humidity').innerHTML = "Humidity :" + d.main.humidity;
-}
-
-
-/*
-/// This awful code is what draws the icon.
+    /// This awful code is what draws the icon.
 /// Sets the visual image depending on the current weather. //
+var currentweather = d.weather[0].description
+
 if (currentweather === "Clear") {
     var currentIcon = $('<img>').attr("src", "http://openweathermap.org/img/wn/01d.png");
     currentIcon.attr("style", "height: 60px; width: 60px");
@@ -78,49 +79,65 @@ if (currentweather === "Clear") {
 } else if (currentweather === "Fog") {
     var currentIcon = $('<img>').attr("src", "http://openweathermap.org/img/wn/50d.png");
     currentIcon.attr("style", "height: 60px; width: 60px");
-}
-/// ^ This crap is literally Yanderedev levels of trash, but it gets the job done... I guess. 
 };
-*/
+/// ^ This crap is literally Yanderedev levels of trash, but it gets the job done... I guess. 
+
+// Temperature
+    var celcius = Math.round(parseFloat(d.main.temp) - 273.15);
+    var fahrenheit = Math.round(((parseFloat(d.main.temp) - 273.15) * 1.8) + 32);
+
+    document.getElementById('description').innerHTML = d.weather[0].description;
+    document.getElementById('temp').innerHTML = celcius + '&deg;';
+    document.getElementById('temp2').innerHTML = fahrenheit + '&deg;';
+    document.getElementById('location').innerHTML = d.name;
+    document.getElementById('humidity').innerHTML = "Humidity :" + d.main.humidity;
+}
+
+function showForecast() {
+return;
+}
 
 
-$("setCity").on("click", function(event) {
-    
-    var cityName = document.getElementById('cityInput').value
 
-    console.log(cityName)
+// Submission button on click event
 
+$("#setCity").on("click", function (event) {
+
+    var cityName = document.getElementById('cityInput').value;
+
+    console.log(cityName);
+    pageLoad();
 
     // Preventing the button from trying to submit the form......
     event.preventDefault();
     // Storing the city name........
-    var buildCity = $(cityName).val().trim();
+    var buildCity = $(cityName).val();
 
     //save search term to local storage.....
     var textContent = $(this).siblings("input").val();
     var storearr = [];
+
     storearr.push(textContent);
-    localStorage.setItem('cityName', JSON.stringify(storearr));
+    localStorage.setItem(cityName, JSON.stringify(storearr));
 
     searchCity(buildCity);
     pageLoad();
+
 });
 
 //---------------------------Call stored items on page load-------------------------------------//
 
-//Event deligation...
-$("#searchhistory").on('click', '.btn', function(event) {
+// Event deligation...
+$("#searchhistory").on('click', '.btn', function (event) {
     event.preventDefault();
     console.log($(this).text());
     searchCity($(this).text());
 
 });
 
-
-
 //// Storage
 function pageLoad() {
-    var lastSearch = JSON.parse(localStorage.getItem("cityName"));
+    var lastSearch = JSON.parse(localStorage.getItem("#cityName"));
     var searchDiv = $("<button class='btn border text-muted mt-1 shadow-sm bg-white rounded' style='width: 12rem;'>").text(lastSearch);
     var psearch = $("<div>");
     psearch.append(searchDiv)
@@ -137,17 +154,17 @@ function showElements() {
     $('#content').show;
 }
 
-
 hideElements();
+
 
 // Preloader set up //
 
 
-$(document).ready(function() {
+$(document).ready(function () {
 
     // Toggles visibility of elements.
 
-    preloaderFadeOutTime = 1000;
+    preloaderFadeOutTime = 1500;
 
     function hidePreloader() {
         var preloader = $('.preloader');
